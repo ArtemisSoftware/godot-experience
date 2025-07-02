b8738d9996f274ccc73023fcbbaabc4db41938a0
gustavo <gustavo.isaac@gmail.com>
2025-07-02 18:17:32 +0100

    Jump movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 
+@export var speed = 300
+@export var gravity = 30
+@export var jump_force = 300
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,9 +16,17 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	
-	var horizontal_direction = Input.get_axis("move_left","move_right")
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+			
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force		
 	
-	velocity.x = 300 * horizontal_direction
+	var horizontal_direction = Input.get_axis("move_left","move_right")
+	velocity.x = speed * horizontal_direction
+	
 	move_and_slide()
 	
 	pass
